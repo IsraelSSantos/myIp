cb76011c114a37c70bb108e87ddb10a555d3c841
Israel S <israelsoares007@outlook.com>
2021-10-15 17:28:14 -0300

    resolve conflicts

--- src/InfoLocate/Card.jsx
+++ src/InfoLocate/Card.jsx
@@ -5,8 +5,12 @@ let Card = (props) => {
   return (
     <>
       <div className="card-container" style={props.style}>
-        <h5> {props.title} </h5>
-        <h2 id={props.id} />
+        <h5>
+          {" "}
+          {props.title}
+          {``}
+        </h5>
+        <h2 id={props.id}>{``}</h2>
       </div>
     </>
   );
